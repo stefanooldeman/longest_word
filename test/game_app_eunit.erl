4c4d665c6e29110a609754d0c096c8ea907f6e5a
stefanooldeman <stefano.oldeman@gmail.com>
2012-11-20 20:44:29 +0100

    use the foreach fixture to reduce code

--- test/game_app_eunit.erl
+++ test/game_app_eunit.erl
@@ -15,24 +15,20 @@ stop({Name, _Supervisor, _Server}) ->
     ok = application:stop(Name),
     timer:sleep(500).
 
-start_test_() ->
-    {setup, 
+all_test_() ->
+    {foreach,
      fun start/0,
      fun stop/1,
-     fun({_Name, Sup, Server}) ->
-        [?_assertEqual(true, is_pid(Sup)),
-         ?_assertEqual(true, is_pid(Server)),
-         ?_assertEqual(undefined, whereis(game_app)),
-         ?_assertEqual({error, {already_started, game}}, application:start(game))]
-     end
+     [fun start_tests/1, 
+      fun stop_tests/1]
     }.
 
-stop_test_() ->
-    {setup, 
-     fun start/0,
-     fun stop/1,
-     fun({Name, _, _}) ->
-        [?_assertEqual(ok, application:stop(Name)),
-         ?_assertEqual({error,{not_started,Name}}, application:stop(Name))]
-     end
-    }.
+start_tests({_Name, Sup, Server}) ->
+    [?_assertEqual(true, is_pid(Sup)),
+     ?_assertEqual(true, is_pid(Server)),
+     ?_assertEqual(undefined, whereis(game_app)),
+     ?_assertEqual({error, {already_started, game}}, application:start(game))].
+
+stop_tests({Name, _, _}) ->
+    [?_assertEqual(ok, application:stop(Name)),
+     ?_assertEqual({error,{not_started,Name}}, application:stop(Name))].
