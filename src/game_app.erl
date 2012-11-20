3068515cfec783dcc8c342f3fe2fb33122a2ef8b
stefanooldeman <stefano.oldeman@gmail.com>
2012-11-20 14:08:06 +0100

    add OPT structure - .erlang with code:add_patha("./ebin") - rebar config and app.src file - Makefile - implementations for following behaviour: application, supervisor and gen_server

---
+++ src/game_app.erl
@@ -0,0 +1,19 @@
+-module(game_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    io:format("Started~n"),
+    game_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+
