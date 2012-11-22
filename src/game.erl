c8035e2c3418b9e4d61dbe86bd4a358891017dbb
stefanooldeman <stefano.oldeman@gmail.com>
2012-11-22 18:15:42 +0100

    re-arrange the file layout

--- src/game.erl
+++ src/game.erl
@@ -2,16 +2,7 @@
 
 -behaviour(gen_server).
 
-%% API
--export([start_link/0]).
-
--type state() :: [any()] | [] | undefined.
--type start_link_error() :: {already_started, pid()} | term().
-
--export([submit/2, get_player/0, get_player/1]).
-
--type player_name() :: atom().
--type player() :: {player_name(), tuple()} | {error, not_found}.
+-export([start_link/0, submit/2, get_player/0, get_player/1]).
 
 %% later this will be private or moved whatever
 -export([get_longest_word/1]).
@@ -19,21 +10,25 @@
 %% Callback functions
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
+-type player_name() :: atom().
+-type player() :: {player_name(), tuple()}.
 -type reply() :: term().
 -type reason() :: term().
 
--spec start_link() -> {ok,pid()} | {error,start_link_error()}.
+
+%%--------------------------------------------------------------------
+%% API (delegated to sync | async calls)
+%%--------------------------------------------------------------------
+
+-spec start_link() -> {ok,pid()} | {error,{already_started, pid()}}.
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
--spec init([]) -> {ok, state()} | {ok, state(), timeout()} | {ok, state(), hibernate} |
+-spec init([]) -> {ok, record()} | {ok, record(), timeout()} | {ok, record(), hibernate} |
     {stop, reason()}.
 init([]) ->
     {ok, []}.
 
-%
-% Public API delegated to sync | async calls
-%
 
 -spec submit(player_name(), string()) -> ok.
 submit(Player, Sentence) -> 
@@ -50,55 +45,60 @@ get_player() -> gen_server:call(?MODULE, all_players).
 get_player(Player) ->
     gen_server:call(?MODULE, {player, Player}).
 
-%%
+%%--------------------------------------------------------------------
 %% Private functions
-%%
+%%--------------------------------------------------------------------
 
 -spec submit_score(player_name(), integer()) -> integer().
 submit_score(Player, Score) ->
    gen_server:call(?MODULE, {submit_score, Player, Score}).
 
+-spec get_longest_word(string()) -> string().
 get_longest_word(Sentence) ->
     List=[{X, length(X)} || X <- string:tokens(Sentence, " ")],
     [{Word,_}|_]=lists:reverse(lists:keysort(2,List)),
     Word.
 
-%%
+%%--------------------------------------------------------------------
 %% Callback functions for gen_server
-%%
--spec handle_call(_, {pid(),_}, state()) -> 
-    {noreply,state()}       | {noreply,state(),hibernate | timeout()} |
-    {reply,reply(),state()} | {reply,reply(),state(),hibernate | timeout()} |
-    {stop,reason(),state()} | {stop,reason(),reply(),state()}.
-handle_call(all_players, _From, State) ->
-    Reply=[],
-    {reply, Reply, State};
+%%--------------------------------------------------------------------
+-spec handle_call(_, {pid(),_}, record()) -> 
+    {noreply,record()}       | {noreply,record(),hibernate | timeout()} |
+    {reply,reply(),record()} | {reply,reply(),record(),hibernate | timeout()} |
+    {stop,reason(),record()} | {stop,reason(),reply(),record()}.
+handle_call(all_players, _From, #state{players=Players}=State) ->
+    {reply, Players, State};
 
-handle_call({player, Player}, _From, State) ->
-    %Reply={Player, {}},
-    Reply={error, not_found},
-    {reply, Reply, State};
-
-handle_call({submit_score, Player, Score}, _From, State) ->
-    %handle state shit here
-    {reply, Score, State};
+handle_call({submit_score, PlayerName, Score}, _From, #state{players=Players,scores=Scores}=State) ->
+    %make sure user is updated
+    case lists:keyfind(PlayerName, 1, Players) of
+        false ->
+            %create if not present
+            PlayersList= [{PlayerName, {}}|Players];
+        T ->
+            %TODO update player ranking and times played ;)
+            PlayersList= lists:keyreplace(PlayerName, 1, Players, T)
+    end,
+    % add highscore shit here
+    ScoresList=[Score|Scores],
+    {reply, Score, State#state{players=PlayersList,scores=ScoresList}};
 
 handle_call(_Request, _From, State) ->
     Reply = ok,
     {reply, Reply, State}.
 
--spec handle_cast(_, state()) -> {noreply, _}.
+-spec handle_cast(_, record()) -> {noreply, _}.
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
--spec handle_info(_, state()) -> {noreply, _}.
+-spec handle_info(_, record()) -> {noreply, _}.
 handle_info(_Info, State) ->
     {noreply, State}.
 
--spec terminate(reason(), state()) -> ok.
+-spec terminate(reason(), record()) -> ok.
 terminate(_Reason, _State) ->
     ok.
 
--spec code_change(_, state(), _) -> {ok, _}.
+-spec code_change(_, record(), _) -> {ok, _}.
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
